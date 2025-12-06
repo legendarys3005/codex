776a7b1bc2e08722363bc0b5df358a5f18f4ad4f
legendarys3005 <legendarys3005@gmail.com>
2025-12-06 22:48:58 +0530

    searchbar logic

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const body = document.getElementById("body");
 const languages = [["HTML", "assets/html.png", "html"], ["CSS", "assets/css.png", "css"], ["Kotlin", "assets/kotlin.png", "kotlin"]];
-for (let language of languages){
+for (let language of languages) {
     const title = language[0];
     const logoPath = language[1];
     const link = language[2]
     const languageButton = createLanguageButton(logoPath, title);
+    languageButton.id = title.toLowerCase();
     body.appendChild(languageButton);
     languageButton.addEventListener("click", () => {
         window.open(link, "_self")
     });
 }
 
+function searchUpdate() {
+    const searchbar = document.getElementById("search");
+    const searchedLanguage = languages.filter(element => {
+        const name = element[0].toLowerCase();
+        return name.includes(searchbar.value.toLowerCase());
+    });
 
+    for (let language of languages) {
+        const id = language[0].toLowerCase();
+        const element = document.getElementById(id);
+        if (searchedLanguage.includes(language)) {
+            element.style.display = "flex";
+        } else {
+            element.style.display = "none";
+        }
+    }
+}
 
 function createLanguageButton(logoPath, title) {
     const languageButton = document.createElement("div");
@@ -19,7 +36,6 @@ function createLanguageButton(logoPath, title) {
     const logo = document.createElement("img");
     logo.src = logoPath;
     logo.alt = title;
-    logo.id = title;
     logo.classList.add("logo");
     const titleTag = document.createElement("h3");
     titleTag.classList.add("title");
@@ -27,6 +43,5 @@ function createLanguageButton(logoPath, title) {
     titleTag.appendChild(titleData);
     languageButton.appendChild(logo);
     languageButton.appendChild(titleTag);
-    console.log(languageButton);
     return languageButton;
 }
